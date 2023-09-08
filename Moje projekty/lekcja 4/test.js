52f90ced820bcebed40a307a7cd188c39ab30419
Marcin Rusiniak <marcin.rusiniak@gmail.com>
2023-09-08 14:39:44 +0200

    test przeniesienie

---
+++ Moje projekty/lekcja 4/test.js
@@ -0,0 +1,57 @@
+/*class User {
+    constructor(name) {
+        this.name = name;
+    }
+    speak() {
+        console.log(this.name + " mówi 123");
+    }
+}
+
+const ania = new User("Ania");
+const zbyszek = new User("Zbyszek");
+const endrju = new User("Andrzej jest stary");
+ania.speak();
+zbyszek.speak();
+endrju.speak();
+
+const users = [ania, zbyszek, endrju];
+
+users.forEach(user => {
+    user.speak();
+})*/
+
+const tab = [1,2,3,4,5,6];
+console.log(tab);
+
+let x = 10;
+console.log(x + 2);
+console.log(x*2);
+console.log(x/2);
+console.log(x%2);
+
+
+for(let i=0;i<100;i++);
+    console.log("Cześć",);
+
+
+for(let i=0;i<7;i++){
+    console.log("Wykonywanie pętli" , i);
+}
+
+class Enemy{
+    constructor(x,y){
+        this.x=x;
+        this.y = y;
+        this.type = "normalny";
+    }
+    fly() {
+        return this.type + "- lecę";
+        
+    }
+}
+ 
+
+
+
+
+
