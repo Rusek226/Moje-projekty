9cd4a80175f9bcd5544381d735055592df6e131e
Marcin Rusiniak <marcin.rusiniak@gmail.com>
2023-09-20 14:21:45 +0200

    20.09.2023

---
+++ Moje projekty/test3.js
@@ -0,0 +1,12 @@
+// alert("treść komunikatu");
+
+confirm("jakieś potwierdzenie");
+
+if (confirm("Czy jesteś pewnien,że chcesz kontynuować?")){
+    alert("No to kontynuuj...");
+
+} else {
+    alert("Przykro mi , że nie chcesz kontunuować...");
+}
+
+prompt("treść komunikatu");
